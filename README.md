304e71f59b66ae37ea60e71715bb32e1ea284637
지운 김 <kkimj@hanyang.ac.kr>
2021-03-23 00:14:25 +0900

    update repository for 1.0.0

--- README.md
+++ README.md
@@ -1,2 +1,51 @@
 # USBSerial_Python-library
 USBSerial_Python-library
+
+
+https://pypi.org/project/USBSerial
+
+## Getting Started
+
+### Installation
+```
+$ pip install USBSerial
+```
+
+## Usage
+
+### Import
+```python
+from usbserial import USBSerial
+```
+
+### Examples
+```python
+from usbserial import USBSerial
+device = USBSerial(port = 'ttyUSB0', baudrate = 115200, timeout = 3, open = True)
+print(device.readline())
+print(device.read(size = 1))
+```
+
+
+## Dev
+
+### Build
+```
+$ python3 -m build
+```
+
+### Local test
+```
+$ pip install -e .
+```
+
+#### Build and Local test
+```
+$ python3 -m build && pip install -e . && python
+```
+
+### Release
+```
+$ python -m twine upload dist/*
+```
+
---
+++ setup.py
@@ -0,0 +1,41 @@
+import setuptools
+
+
+install_requires = [
+    'pyserial==3.5',
+]
+
+try:
+    # $ pip install pypandoc
+    import pypandoc
+    long_description = pypandoc.convert('README.md', 'rst')
+except(IOError, ImportError):
+    long_description = open('README.md').read()
+
+
+setuptools.setup (
+    name = 'USBSerial',
+    version = '1.0.0',
+    license = 'GPL-3.0 License',
+    description = 'HC-USBSerial Python library via Serial protocol, using pySerial library as API',
+    long_description = long_description,
+    long_description_content_type='text/markdown',
+    author = 'KKimj',
+    author_email = 'kkimj@hanyang.ac.kr',
+    url = 'https://github.com/KKimj/USBSerial_Python-library',
+
+    install_requires=install_requires,
+
+    py_modules=["usbserial"],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    classifiers = [
+        # https://pypi.org/classifiers/
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+        "Topic :: Terminals :: Serial",
+        "Natural Language :: Korean",
+    ],
+    python_requires='>=3',
+)
